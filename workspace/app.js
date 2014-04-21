27800c483dcc8e10bdee4ae21f6394a0945e0e81
SailorZhang <z@zhangxiangnan.cn>
2014-04-21 07:58:44 +0000

    use express 4.x

--- workspace/app.js
+++ workspace/app.js
@@ -2,26 +2,33 @@ var http = require("http"),
     fs = require("fs"),
     path = require("path"),
     express = require("express"),
-    app = express(),
-    routes = require('./routes/config');
+    routes = require('./routes/config'),
+    connect = require('connect'),
+    app = express();
     //io = require("scoket.io");
-app.configure(function(){
+//app.configure(function(){
   app.set('port',process.env.PORT || 8080);
   app.set('views',__dirname+'/views');
   app.set('view engine','html');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname,'public')));
-});
+connect().use(require('static-favicon'));
+  //app.use(express.favicon());
+  //app.use(express.logger('dev'));
+  //app.use(express.bodyParser());
+  //app.use(express.methodOverride());
+  //app.use(express.cookieParser('your secret here'));
+  //app.use(express.session());
+  //app.use(app.router);
+  //app.use(express.static(path.join(__dirname,'public')));
+//});
 
-app.configure('development',function(){
-  app.use(express.errorHandler());
-});
+var env = process.env.NODE_ENV || 'development';
+if ('development' == env) {
+  console.log('development');
+}
+
+//app.configure('development',function(){
+  //app.use(express.errorHandler());
+//});
 
 routes(app);
 
