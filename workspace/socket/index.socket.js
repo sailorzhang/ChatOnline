7ad45f091b8dc128e98af0548a67e1024a62031d
SailorZhang <z@zhangxiangnan.cn>
2014-04-22 09:09:42 +0000

    log users

--- workspace/socket/index.socket.js
+++ workspace/socket/index.socket.js
@@ -1,5 +1,24 @@
-exports.beginSocket = function(io){
-  io.sockets.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-  });
+exports.beginSocket = function (io) {
+    var _ = require('underscore');
+    var users = [];
+    io.sockets.on('connection', function (socket) {
+        console.log('connection');
+        socket.emit('connection');
+
+        socket.on('IN', function (data) {
+            console.log('IN');
+            users.push(data.username);
+            socket.set('username', data.username, function () {
+                io.sockets.emit("RefreshUsers", { UserList: users });
+            });
+        });
+
+        socket.on('disconnect', function () {
+            console.log('disconnect');
+            socket.get('username', function (err, username) {
+                users = _.without(users, username);
+                io.sockets.emit("RefreshUsers", { UserList: users });
+            });
+        });
+    });
 }
